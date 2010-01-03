7c0b311c522fcfd11b74d352a9798de9ec0ed5e1
Luke Lee <durdenmisc@gmail.com>
2010-01-03 17:38:03 -0600

    Adding docstrings for form

--- lastfm/findshows/forms/forms.py
+++ lastfm/findshows/forms/forms.py
@@ -1,6 +1,11 @@
+"""Form classes for lastfm show searches"""
+
 from django import forms
 
+
 class ShowsForm(forms.Form):
+    """Form to collect lastfm username and city/state for upcoming shows"""
+
     username = forms.CharField(max_length=100)
     city = forms.CharField(max_length=50)
     state = forms.CharField(max_length=2)
