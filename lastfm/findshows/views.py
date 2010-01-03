dc912172bf8616257f4f0acd43b8a950fa11b2d3
Luke Lee <durdenmisc@gmail.com>
2010-01-03 17:38:42 -0600

    Cleaning up some pep8 standards and pylint issues found (docstrings, variable shadowing, etc.)

--- lastfm/findshows/views.py
+++ lastfm/findshows/views.py
@@ -1,4 +1,5 @@
-# Create your views here.
+"""Views for viewing upcoming show for favorite lastfm bands"""
+
 from django.shortcuts import render_to_response
 from lastfm.findshows.forms.forms import ShowsForm
 
@@ -10,24 +11,34 @@ import eventful
 API_KEY = "2465572f9a20dc3775129665207edfcf"
 API_SECRET = "ed9ccfacd5df88cf05c0d58fb546481c"
 
+
 def home(request):
-    form = ShowsForm()
-    return render_to_response('home.html', {'form' : form})
+    """Show base form to view upcoming shows"""
+
+    if request.method == 'GET':
+        form = ShowsForm()
+        return render_to_response('home.html', {'form': form})
+
 
 def myshows(request):
+    """Handle request for upcoming shows"""
+
     if request.method == 'POST':
         form = ShowsForm(request.POST)
         if form.is_valid():
             user = form.cleaned_data['username']
             city = form.cleaned_data['city']
             state = form.cleaned_data['state']
-            shows = __find_shows__(user, city, state)
-            return render_to_response('myshows.html', {'myshows' : shows,
-                                      'user' : user})
+            shows = __find_shows(user, city, state)
+            return render_to_response('myshows.html', {'myshows': shows,
+                                      'user': user})
 
     return render_to_response('home.html')
 
-def __find_shows__(lastfm_user, city, state):
+
+def __find_shows(lastfm_user, city, state):
+    """Look up top bands for lastfm user and check eventful for shows"""
+
     user = pylast.User(user_name=lastfm_user, api_key=API_KEY,
                        api_secret=API_SECRET, session_key=None)
     api = eventful.API(app_key="hdpzLJWDRbFWFsLf")
@@ -37,19 +48,19 @@ def __find_shows__(lastfm_user, city, state):
     except pylast.ServiceException:
         return None
 
-    myshows = {}
+    shows = {}
 
     for band in bands[:20]:
         bname = band.get_item().get_name().encode('utf-8')
         per = 'performer:"%s"' % (bname)
         loc = "%s, %s" % (city, state)
-        shows = api.call(method="events/search", location=loc, keywords=per)
+        res = api.call(method="events/search", location=loc, keywords=per)
 
-        if int(shows['total_items']) > 0:
-            myshows[bname] = []
-            for show in shows['events']['event']:
+        if int(res['total_items']) > 0:
+            shows[bname] = []
+            for show in res['events']['event']:
                 time = datetime.datetime.strptime(show['start_time'],
                                                   '%Y-%m-%d %H:%M:%S')
-                myshows[bname].append({'venue' : show['venue_name'],
-                                       'time' : time})
-    return myshows
+                shows[bname].append({'venue': show['venue_name'],
+                                       'time': time})
+    return shows
